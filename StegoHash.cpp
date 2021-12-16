0eae63e5dc234a28d263815d31d1cfcd54fcf2e9
Ivandows <tal200@lenta.ru>
2021-12-16 15:04:21 +0700

    Добавлены unit тесты.

---
+++ StegoHash.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include "BitStr.h"
+#include <string.h>
+#include "gtest/gtest.h"
+
+
+int main(int argc,char *argv[])
+{	
+	testing::InitGoogleTest(&argc,argv);
+	return RUN_ALL_TESTS();
+}
